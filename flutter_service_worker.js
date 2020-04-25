ef9f96620b07a943084a1bd58a1512566ead5767
Anurag <anuragr9847@gmail.com>
2020-04-25 13:34:23 +0530

    Add animated AppBar

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,14 +4,14 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "15e3b196ed5d94f4a41d0304f6b8a680",
+"assets/LICENSE": "d5abe22ec2e1c9780168e20599f48541",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "194bbdb13932cf70330b3b83edcd7cb1",
 "/": "194bbdb13932cf70330b3b83edcd7cb1",
-"main.dart.js": "9bbb838258533570b0c6f31f79e57449",
+"main.dart.js": "8881cf70ba468850d8b74c7849439fe3",
 "manifest.json": "ecca3acc7f1180cb78ff7cac0319ccdc"
 };
 
